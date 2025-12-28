c831ae94c255fb0d538190001393cb536ca80969
ImmatureGoat <63555728+octaviustheking@users.noreply.github.com>
2025-12-28 16:00:41 -0500

    bruh i can't do this

---
+++ python/codeforces/B. Ashmal/main.py
@@ -0,0 +1,12 @@
+times = int(input())
+
+for i in range(times):
+    bruh = input()
+    strings = input().split()
+    final_string = ""
+    for string in strings:
+        if string < final_string:
+            final_string = string + final_string
+        else:
+            final_string = final_string + string
+    print(final_string)
